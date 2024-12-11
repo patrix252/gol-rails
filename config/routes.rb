fca25ddb16d27a08e899fb220a46aafc5e2a4553
Patrick Peterle <patrix252@gmail.com>
2024-12-11 16:45:12 +0100

    feat file upload

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "pages#index"
+
+  get "new" => "pages#new"
+  post "upload" => "pages#upload"
+  get "play" => "pages#play"
 end
